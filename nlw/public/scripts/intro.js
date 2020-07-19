e5466a7015e75ae2248f132719dbd11b2aab6952
Larissa Siqueira <64505863+LarissaSiq@users.noreply.github.com>
2020-07-19 00:54:44 -0300

    Add files via upload

---
+++ nlw/public/scripts/intro.js
@@ -0,0 +1,69 @@
+// coments
+// document.write("Hello")
+
+// variaveis, tipos de dados
+// myvar = "He"
+// const myconst = "He"
+
+// document.write(myconst + myvar)
+
+// string
+// "Isso é uma string"
+// 'Isso também é uma string'
+// `Isso é uma string também`
+
+// number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+// objeto possuem propriedades e funcionalidade
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     document.write("executar uma grande logica de correr")
+//   }
+// }
+
+// pessoa.correr()
+
+// Array - Vetores
+// const colecaoDeBolinhas = ["blue", "green", 1, {name:"My Name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+
+// Funções - Funcionalidades - Atalhos - Reuso de código
+
+// registrar função
+// function sayMyName(name) {
+
+//   document.write(name)
+// }
+
+// executar
+// sayMyName("Siqueira")
+// sayMyName("Kyam")
+// sayMyName("Jorge")
+
+// condicionais
+
+// const notaFinal = 4
+
+//if( notaFinal < 5 ) {
+//   document.write("Reprovado")
+// } else {
+//   document.write("Aprovado")
+// }
+
+// loop repetições
+// for (i = 0; i < 10; i++) {
+//   document.write(`<p>${i}</p>`)
+// }
